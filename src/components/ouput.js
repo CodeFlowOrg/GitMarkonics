dc64994963b082c28c301e4e585901b518c72452
hkaur008 <hargunalone@gmail.com>
2021-03-14 12:21:11 +0530

    markdown previewer added

---
+++ src/components/ouput.js
@@ -0,0 +1,48 @@
+import marked from 'marked';
+import {
+  Text,
+  Box,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
+
+const Output = (file) => {
+ 
+  
+const getMarkdownText= ()=>{
+    var rawMarkup= marked('_Nothing_ to show ') ;
+    if(file.file){rawMarkup=marked(file.file);
+      console.log(file.file);
+      console.log()
+    }
+    else{
+      rawMarkup = marked('_Nothing_ to show ');
+    }
+    return { __html: rawMarkup };
+  }
+  return (
+    <Box flex="1" bg="white" border="1px" borderColor="gray.10" p={5}>
+      <Tabs>
+        <TabList>
+          <Tab>Output</Tab>
+          <Tab>Preview</Tab>
+         
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <Text>{file.file}</Text>
+          </TabPanel>
+          <TabPanel>
+         <div dangerouslySetInnerHTML={getMarkdownText()} />
+          </TabPanel>
+         
+        </TabPanels>
+      </Tabs>
+    </Box>
+  );
+};
+export default Output;
--- src/components/texteditor.js
+++ src/components/texteditor.js
@@ -1,85 +1,99 @@
-import React, {Component} from 'react'; 
-// Importing Module 
-import {Editor, EditorState , RichUtils} from 'draft-js';
-import 'draft-js/dist/Draft.css';
-import './texteditor.css';
-import {
-  convertToRaw,
-} from 'draft-js';
+import React, { Component } from "react";
+// Importing Module
+import { Editor, EditorState, RichUtils } from "draft-js";
+import "draft-js/dist/Draft.css";
+import "./texteditor.css";
+import { convertToRaw } from "draft-js";
 import { stateToHTML } from "draft-js-export-html";
-import { Flex,Center,Text,Box,Button,Stack,Icon} from "@chakra-ui/react";
-import { GoBold,GoItalic,GoDesktopDownload } from "react-icons/go";
+import { Flex, Center, Text, Box, Button, Stack, Icon } from "@chakra-ui/react";
+import { GoBold, GoItalic, GoDesktopDownload } from "react-icons/go";
+import Output from './ouput'
 
-const  dkl = "hello";
- export default class Texteditor extends Component {
+export default class Texteditor extends Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = editorState => {this.setState({editorState});
-     var value =stateToHTML(this.state.editorState.getCurrentContent());
-    this.file=this.replacer(value);
-  };
- 
+    this.state = { editorState: EditorState.createEmpty() };
+    this.onChange = (editorState) => {
+      this.setState({ editorState });
+      var value = stateToHTML(this.state.editorState.getCurrentContent());
+      this.file = this.replacer(value);
+      // console.log(convertToRaw(this.state.editorState.getCurrentContent()));
+    };
+  }
 
+  replacer(input) {
+    // console.log(input);
+    input = input
+      .replace(/<p>/g, "")
+      .replace(/<\/p>/g, "</br>")
+      .replace(/<br>/g, "")
+      .replace(/<strong>/g, "**")
+      .replace(/<\/strong>/g, "**")
+      .replace(/&nbsp;/g, " ");
+    input = input.replace(/<em>/g, "_").replace(/<\/em>/g, "_");
+    return input;
   }
-  replacer(input)
-  {
- console.log(input);
- input=input.replace(/<p>/g, ' ').replace(/<\/p>/g, '</br>').replace(/<strong>/g, '**').replace(/<\/strong>/g, '**').replace(/&nbsp;/g," ");  
- 
- return input;
-  }
- 
+
   _onBoldClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-    
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "BOLD"));
   }
   _onItalicClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+    this.onChange(
+      RichUtils.toggleInlineStyle(this.state.editorState, "ITALIC")
+    );
   }
   _onUnderlineClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
-    
+    this.onChange(
+      RichUtils.toggleInlineStyle(this.state.editorState, "UNDERLINE")
+    );
   }
-  onDownload(){
-   
-   const hiddenElement = document.createElement('a');
+  onDownload() {
+    const hiddenElement = document.createElement("a");
 
-   hiddenElement.href = 'data:attachment/text,' + encodeURI(this.file);
-   hiddenElement.target = '_blank';
-   hiddenElement.download = 'README.md';
-   hiddenElement.click();
-    
+    hiddenElement.href = "data:attachment/text," + encodeURI(this.file);
+    hiddenElement.target = "_blank";
+    hiddenElement.download = "README.md";
+    hiddenElement.click();
   }
-    
-    
-      render() {
-        return (
-<div>
-<Flex color="black" >
-  
-  <Box  flex="1" bg="tomato" border="1px" borderColor="gray.10"  p={1} m={2} >
-  <Stack direction="row" spacing={1} align="center">
-  <Button onClick={this._onBoldClick.bind(this)}><Icon as={GoBold} />
-    </Button>
-          <Button  onClick={this._onItalicClick.bind(this)}> <Icon as={GoItalic} /></Button>
-          
-          <Button  onClick={this.onDownload.bind(this)}><Icon as={GoDesktopDownload} /></Button>
-          </Stack>
+
+  render() {
+    return (
+      <div>
+        <Flex color="black">
+          <Box
+            flex="1"
+            bg="tomato"
+            border="1px"
+            borderColor="gray.10"
+            p={1}
+            m={2}
+          >
+            <Stack direction="row" spacing={1} align="center">
+              <Button onClick={this._onBoldClick.bind(this)}>
+                <Icon as={GoBold} />
+              </Button>
+              <Button onClick={this._onItalicClick.bind(this)}>
+                {" "}
+                <Icon as={GoItalic} />
+              </Button>
+
+              <Button onClick={this.onDownload.bind(this)}>
+                <Icon as={GoDesktopDownload} /> &nbsp;Download
+              </Button>
+            </Stack>
             <Box className="editors-panel">
-              <Editor editorState={this.state.editorState}
-                  handleKeyCommand={this.handleKeyCommand}
-                  onChange={this.onChange} /></Box>
-  </Box>
-  <Box flex="1" bg="white" border="1px" borderColor="gray.10" p={5}>
-    <Text>
-       OUTPUT
-    </Text>
-    <Text> {this.file}</Text>
-  </Box>
-</Flex>
-</div>
-
-        );
-    }
+              <Editor
+                editorState={this.state.editorState}
+                handleKeyCommand={this.handleKeyCommand}
+                onChange={this.onChange}
+                placeholder="Lets Start Documenting ..."
+              />
+            </Box>
+          </Box>
+          <Output file ={this.file}/>
+        </Flex>
+         
+      </div>
+    );
+  }
 }
