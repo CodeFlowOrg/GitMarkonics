588930553c98acb5a90e1ed09e20d87853ad371d
hkaur008 <hargunalone@gmail.com>
2021-04-01 20:27:44 +0530

    homepage link updated

--- src/components/nav-head.js
+++ src/components/nav-head.js
@@ -1,13 +1,46 @@
 import "./nav-head.css";
 import { GrLogin } from "react-icons/gr";
-import { Button, Spacer, Flex, Stack, Box } from "@chakra-ui/react";
-import logo from '../assets/white heading.png'
+import {
+  Button,
+  Spacer,
+  Flex,
+  Stack,
+  Box,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  IconButton,
+} from "@chakra-ui/react";
+import { BiMenu } from "react-icons/bi";
+import logo from "../assets/white heading.png";
 
 function NavHeader() {
   return (
     <Box className="nav_header" p={1}>
       <Flex direction="row" spacing={2}>
-        <img src={logo} alt="heading"  width="120px" class="center"/>
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={BiMenu}
+            bg="#F0A6CA"
+            border="0.5px"
+            borderColor="#F0E6EF"
+            p={1}
+            m={2}
+            size="xs"
+            variant="solid"
+            m={1}
+          />
+          <MenuList>
+            <MenuItem command="⌘T">New Tab</MenuItem>
+            <MenuItem command="⌘N">New Window</MenuItem>
+            <MenuItem command="⌘⇧N">Open Closed Tab</MenuItem>
+            <MenuItem command="⌘O">Open File...</MenuItem>
+          </MenuList>
+        </Menu>
+        <img src={logo} alt="heading" width="120px" class="center" />
         <Spacer />
         <Stack direction="row" spacing={2}>
           <Button leftIcon={<GrLogin />} colorScheme="pink" variant="solid">
