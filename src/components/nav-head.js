45028723f4d0ecf87d335757cc01ad82bcb40959
Yash natani <yash_11912010@nitkkr.ac.in>
2021-04-03 17:09:37 +0530

    cleaned the code

--- src/components/nav-head.js
+++ src/components/nav-head.js
@@ -5,41 +5,16 @@ import {
   Spacer,
   Flex,
   Stack,
-  Box,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  IconButton,
+  Box
 } from "@chakra-ui/react";
-import { BiMenu } from "react-icons/bi";
+
 import logo from "../assets/GitMarkonics_white.png";
 
 function NavHeader() {
   return (
     <Box className="nav_header" p={1}>
       <Flex direction="row ">
-        {/* <Menu>
-          <MenuButton
-            as={IconButton}
-            aria-label="Options"
-            icon={BiMenu}
-            bg="#F0A6CA"
-            border="0.5px"
-            borderColor="#F0E6EF"
-            p={1}
-            m={2}
-            size="xs"
-            variant="solid"
-            m={1}
-          />
-          <MenuList>
-            <MenuItem command="⌘T">New Tab</MenuItem>
-            <MenuItem command="⌘N">New Window</MenuItem>
-            <MenuItem command="⌘⇧N">Open Closed Tab</MenuItem>
-            <MenuItem command="⌘O">Open File...</MenuItem>
-          </MenuList>
-        </Menu> */}
+       
         <img src={logo} alt="heading" width="80px" />
         <Spacer />
         <Stack direction="row" spacing={2}>
