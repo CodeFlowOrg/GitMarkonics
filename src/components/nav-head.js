8ccec1167999bc4585aaaf729c76e4094d8f6624
Rohan Modi <rohanmodi790@gmail.com>
2021-07-18 18:10:44 +0530

    ScrollBar Color Changed alongwith contact form sucessfully added

--- src/components/nav-head.js
+++ src/components/nav-head.js
@@ -7,26 +7,48 @@ import { Link } from "react-router-dom";
 import logo from "../assets/GitMarkonics_white.png";
 
 function NavHeader() {
-  return (
-    <Box className="nav_header" p={1}>
-      <Flex direction="row ">
-        <img src={logo} alt="heading" width="80px" />
-        <Spacer />
-        <Stack direction="row" spacing={2}>
-          <Link to="/login">
-            <Button leftIcon={<GrLogin />} colorScheme="pink" variant="solid">
-              Login
-            </Button>
-          </Link>
-          <Link to="/register">
-          <Button colorScheme="white" variant="outline">
-            Register
-          </Button>
-          </Link>
-        </Stack>
-      </Flex>
-    </Box>
-  );
+    return ( <
+        Box className = "nav_header"
+        p = { 1 } >
+        <
+        Flex direction = "row " >
+        <
+        img src = { logo }
+        alt = "heading"
+        width = "80px" / >
+        <
+        Spacer / >
+        <
+        Stack direction = "row"
+        spacing = { 2 } >
+        <
+        Link to = "/login" >
+        <
+        Button leftIcon = { < GrLogin / > }
+        colorScheme = "pink"
+        variant = "solid" >
+        Login <
+        /Button> < /
+        Link > <
+        Link to = "/register" >
+        <
+        Button colorScheme = "white"
+        variant = "outline" >
+        Register <
+        /Button> < /
+        Link > <
+        Link to = "/contactus" >
+        <
+        Button leftIcon = { < GrLogin / > }
+        colorScheme = "blue"
+        variant = "solid" >
+        ContactUs <
+        /Button> < /
+        Link > <
+        /Stack> < /
+        Flex > <
+        /Box>
+    );
 }
 
 export default NavHeader;
